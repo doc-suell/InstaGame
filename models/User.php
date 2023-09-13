47f4e620859de50e4e51b540e49eb857deecaf81
Arnaud Humen <arnaud.valentino.humen@gmail.com>
2023-09-13 16:48:05 +0200

    facto

--- models/User.php
+++ models/User.php
@@ -2,7 +2,8 @@
 
 include_once "./config/database.php";
 
-class User {
+class User
+{
     private $db;
     private ?int $id;
     private string $username;
@@ -10,86 +11,91 @@ class User {
     private string $password;
     private string $profilePicture;
     private string $registrationDate;
-   
+
 
     // Constructeur
-    public function __construct(?int $id ,  $username, $email, $password, $profilePicture,$registrationDate) {
+    public function __construct(?int $id,  $username, $email, $password, $profilePicture, $registrationDate)
+    {
         $this->id = $id;
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
         $this->profilePicture = $profilePicture;
         $this->registrationDate = $registrationDate;
-        $this->db = new Database();
-        
     }
 
-    public static function getUser() {
-        $db = new Database();
+    public static function getUser(Database $db)
+    {
         $stmt = $db->getConnection()->query('SELECT * FROM users');
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function getsById(int $id) {
-        $db = new Database();
+    public static function getsById(int $id, Database $db)
+    {
         $stmt = $db->getConnection()->query("SELECT * FROM users WHERE id = $id");
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public static function getsByUsername(string $username) {
-        $db = new Database();
+    public static function getsByUsername(string $username, Database $db)
+    {
         $stmt = $db->getConnection()->query("SELECT * FROM users WHERE username = '$username'");
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public static function getUserByEmail(string $email) {
-        $db = new Database();
+    public static function getUserByEmail(string $email, Database $db)
+    {
         $stmt = $db->getConnection()->prepare('SELECT * FROM users WHERE email = ?');
         $stmt->execute([$email]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-    
-    public static function addUser(string $username, string $email, string $password, string $profilePicture) {
-        $db = new Database();
+
+    public static function addUser(string $username, string $email, string $password, string $profilePicture, Database $db)
+    {
         $stmt = $db->getConnection()->prepare('INSERT INTO users (username, email, password, profile_picture) VALUES (?, ?, ?, ?)');
         $stmt->execute([$username, $email, $password, $profilePicture]);
     }
 
-    public static function updateUser(int $id, string $username, string $email, string $password, string $profilePicture) {
-        $db = new Database();
+    public static function updateUser(int $id, string $username, string $email, string $password, string $profilePicture, Database $db)
+    {
         $stmt = $db->getConnection()->prepare('UPDATE users SET username = ?, email = ?, password = ?, profile_picture = ? WHERE id = ?');
         $stmt->execute([$username, $email, $password, $profilePicture, $id]);
     }
 
-    public static function deleteUser(int $id) {
-        $db = new Database();
+    public static function deleteUser(int $id, Database $db)
+    {
         $stmt = $db->getConnection()->prepare('DELETE FROM users WHERE id = ?');
         $stmt->execute([$id]);
     }
 
-     
+
     // Méthodes getters
-    public function getId(): int {
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function getUsername(): string {
+    public function getUsername(): string
+    {
         return $this->username;
     }
 
-    public function getEmail(): string {
+    public function getEmail(): string
+    {
         return $this->email;
     }
 
-    public function getPassword(): string {
+    public function getPassword(): string
+    {
         return $this->password;
     }
 
-    public function getRegistrationDate(): string {
+    public function getRegistrationDate(): string
+    {
         return $this->registrationDate;
     }
 
-    public function getProfilePicture(): string {
+    public function getProfilePicture(): string
+    {
         return $this->profilePicture;
     }
 
@@ -98,7 +104,7 @@ class User {
      * Set the value of id
      *
      * @return  self
-     */ 
+     */
     public function setId(int $id)
     {
         $this->id = $id;
@@ -110,7 +116,7 @@ class User {
      * Set the value of username
      *
      * @return  self
-     */ 
+     */
     public function setUsername(string $username)
     {
         $this->username = $username;
@@ -122,7 +128,7 @@ class User {
      * Set the value of email
      *
      * @return  self
-     */ 
+     */
     public function setEmail(string $email)
     {
         $this->email = $email;
@@ -134,7 +140,7 @@ class User {
      * Set the value of password
      *
      * @return  self
-     */ 
+     */
     public function setPassword(string $password)
     {
         $this->password = $password;
@@ -146,7 +152,7 @@ class User {
      * Set the value of registrationDate
      *
      * @return  self
-     */ 
+     */
     public function setRegistrationDate(string $registrationDate)
     {
         $this->registrationDate = $registrationDate;
@@ -158,24 +164,11 @@ class User {
      * Set the value of profilePicture
      *
      * @return  self
-     */ 
+     */
     public function setProfilePicture(string $profilePicture)
     {
         $this->profilePicture = $profilePicture;
 
         return $this;
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
