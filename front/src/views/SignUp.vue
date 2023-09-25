986f9be65ef524ff31222e1541321a09dd2bb8bc
doc-suell <souhail888@hotmail.com>
2023-09-25 13:13:28 +0200

    lastOne

--- front/src/views/SignUp.vue
+++ front/src/views/SignUp.vue
@@ -1,3 +1,38 @@
+        <script>
+        
+        import axios from 'axios';
+        
+        export default {
+            data() {
+                return {
+                    formData: {
+                        action: "addMember",
+                        username: "",
+                        email: "",
+                        password: "",
+                    }
+                };
+            },
+            methods: {
+                async submitForm() {
+        
+                    {
+                        try {
+                            const response = await axios.post('http://localhost/instaGame/controller/memberController.php?action=addMember', this.formData, {
+                                headers: {
+                                    'Content-Type': 'application/x-www-form-urlencoded',
+                                },
+                            });
+                            console.log("test",response.data);
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    }
+        
+                }
+            }
+        };
+        </script>
 <template>
     <div class="container-all-forms">
         <div class="form-container">
@@ -25,40 +60,5 @@
         </div>
     </div>
 </template>
-<script>
-
-import axios from 'axios';
-
-export default {
-    data() {
-        return {
-            formData: {
-                action: "addMember",
-                username: "",
-                email: "",
-                password: "",
-            }
-        };
-    },
-    methods: {
-        async submitForm() {
-
-            {
-                try {
-                    const response = await axios.post('http://localhost/instaGame/controller/memberController.php?action=addMember', this.formData, {
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    });
-                    console.log("fffffffff",response.data);
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-
-        }
-    }
-};
-</script>
   
 <style></style>
