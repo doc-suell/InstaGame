09ecff2d0d39b02988abb2daa2be8687b35b08ad
doc-suell <souhail888@hotmail.com>
2023-09-28 13:17:52 +0200

    profile

--- front/src/views/Profil.vue
+++ front/src/views/Profil.vue
@@ -1,13 +1,44 @@
 <template>
     <NavBar  />
-    <div class="container profile-container">
-        <div class="header-profile">
-        <div class="profile">
-            <img :src="profilePicture" alt="profil picture" class="">
+    <div class="flex items-center w-full justify-center">
+
+<div class="max-w-xs">
+    <div class="bg-white shadow-2xl rounded-lg py-3">
+        <div class="photo-wrapper p-2">
+            <img class="w-32 h-32 rounded-full mx-auto" :src="profilePicture" alt="John Doe">
         </div>
-        <p>User Name : <span class="">{{ username }}</span></p> 
-        <span>{{ email }}</span>
+        <div class="p-2">
+            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">{{ username }}</h3>
+            <div class="text-center text-gray-400 text-xs font-semibold">
+                <p>Web Developer</p>
+            </div>
+            <table class="text-xs my-3">
+                <tbody><tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                    <td class="px-2 py-2">xxxx - Bordeaux Dawan </td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Phone</td>
+                    <td class="px-2 py-2">+33 9955221114</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
+                    <td class="px-2 py-2">john@exmaple.com</td>
+                </tr>
+            </tbody></table>
+        </div>
     </div>
+</div>
+
+</div>
+    <!-- <div class="container profile-container">
+        <div class="header-profile">
+        <div class="profile"> -->
+            <!-- <img :src="profilePicture" alt="profil picture" class=""> -->
+        <!-- </div> -->
+        <!-- <p>User Name : <span class="">{{ username }}</span></p>  -->
+        <!-- <span>{{ email }}</span> -->
+    <div class="container profile-container">
         <div v-if="posts && posts.length > 0">
             <PostCard @postDeleted="handlePostDeleted" :posts="posts" />
         </div>
