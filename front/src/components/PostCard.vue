aa222039e584c59d0d0625a512dddc00c410bb56
Arnaud Humen <arnaud.valentino.humen@gmail.com>
2023-09-28 11:49:07 +0200

    post delete v2

--- front/src/components/PostCard.vue
+++ front/src/components/PostCard.vue
@@ -2,9 +2,8 @@
 import { onMounted } from "vue";
 import Comment from "./Comment.vue";
 import EditPostModal from "./EditPostModal.vue";
-import axios from 'axios';
-import { ref, defineProps } from 'vue'
-
+import axios from "axios";
+import { ref, defineProps } from "vue";
 
 export default {
   name: "PostCard",
@@ -19,16 +18,46 @@ export default {
       selectedPostId: null,
       postProfilPicture: "",
       modalStates: {},
+      currentUser: null,
     };
   },
   methods: {
+    async getCurrentUser() {
+      try {
+        const instance = axios.create({
+          baseURL: "http://localhost/instaGame/controller/",
+          withCredentials: true,
+        });
+
+        const response = await instance.get("commentController.php", {
+          params: {
+            action: "getUserInfo",
+          },
+        });
+        // console.log('Current user:', response.data.username);
+        if (response.data.id) {
+          console.log("Current user:", response.data.id);
+          // Vous pouvez stocker les informations de l'utilisateur dans la variable currentUser
+          this.currentUser = response.data.id;
+          // console.log(this.currentUser);
+        } else {
+          console.error("Unable to get current user information.");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    //---------------------------------//
     async deletePost(postId) {
       try {
-        await axios.delete(`http://localhost/instaGame/controller/postController.php?action=deletePost&id=${postId}`);
-        this.$emit('postDeleted', postId);
-        this.$emit('openModalEdit', postId);
+        await axios.delete(
+          `http://localhost/instaGame/controller/postController.php?action=deletePost&id=${postId}`
+        );
+        this.$emit("postDeleted", postId);
+        this.$emit("openModalEdit", postId);
       } catch (error) {
-        console.error('Erreur lors de la suppression du post :', error);
+        console.error("Erreur lors de la suppression du post :", error);
       }
     },
     openModalEdit() {
@@ -37,18 +66,19 @@ export default {
     closeModalEdit() {
       this.isModalOpenEdit = false;
     },
-    editPost(postId) { // Fonction pour ouvrir la fenêtre modale et stocker l'ID du post sélectionné
+    editPost(postId) {
+      // Fonction pour ouvrir la fenêtre modale et stocker l'ID du post sélectionné
       this.selectedPostId = postId;
       this.openModalEdit();
     },
     toggleSmallModal(postId) {
-    this.modalStates[postId] = !this.modalStates[postId];
+      this.modalStates[postId] = !this.modalStates[postId];
+    },
   },
-
-  },
-
-
-}
+  created() {
+    this.getCurrentUser();
+},
+};
 </script>
 
 <template>
@@ -58,13 +88,21 @@ export default {
       <div class="card-items">
         <div class="card-header">
           <!-- SMALL MODAL  -->
-          <div v-show="modalStates[post.id]" :id="'smallModal_' + post.id" class="small-modal-post">
+          <div
+            v-show="modalStates[post.id]"
+            :id="'smallModal_' + post.id"
+            class="small-modal-post"
+          >
             <!-- <div class="overlay-small-modal"></div> -->
             <ul>
-              <button @click="deletePost(post.id)">
+              <button
+                v-if="post.user_id === currentUser"
+                @click="deletePost(post.id)"
+              >
                 <span>Delete</span>
-                <i class="fa-regular fa-trash-can"></i></button>
-              <button @click="editPost(post.id)"> 
+                <i class="fa-regular fa-trash-can"></i>
+              </button>
+              <button @click="editPost(post.id)">
                 <span>Edit Post</span>
                 <i class="fa-regular fa-pen-to-square"></i>
               </button>
@@ -76,30 +114,41 @@ export default {
           </div>
           <!-- END SMALL MODAL  -->
           <div class="pic-profile-nav">
-            <img src="/assets/images/E-TAfEiWYAI_Qgu.jpg" alt="profile-pic">
+            <img src="/assets/images/E-TAfEiWYAI_Qgu.jpg" alt="profile-pic" />
           </div>
           <span class="user-name">{{ post.username }}</span>
           <!-- SMALL MODAL OPEN  -->
-          <span @click="toggleSmallModal(post.id)" id="toggle-small-modal" class="open-small-modal-post"><i class="fa-solid fa-ellipsis"></i></span>
+          <span
+            @click="toggleSmallModal(post.id)"
+            id="toggle-small-modal"
+            class="open-small-modal-post"
+            ><i class="fa-solid fa-ellipsis"></i
+          ></span>
         </div>
         <div class="card-body">
-          <img :src="post.post_picture" alt="post-pic">
+          <img :src="post.post_picture" alt="post-pic" />
         </div>
         <div class="card-footer">
           <div class="card-footer-icons">
             <i class="fa-regular fa-heart"></i>
             <i class="fa-regular fa-comment"></i>
           </div>
-          <span class="card-footer-icons"><i class="fa-regular fa-bookmark"></i></span>
+          <span class="card-footer-icons"
+            ><i class="fa-regular fa-bookmark"></i
+          ></span>
         </div>
         <p class="description mt-2 text-base">{{ post.description }}</p>
-        <hr class="w-4/5 mx-auto mt-2"/>
+        <hr class="w-4/5 mx-auto mt-2" />
         <div class="comment">
           <Comment :postId="post.id" />
         </div>
       </div>
       <!-- END SINGLE CARD :// -->
-      <EditPostModal :isOpenEdit="isModalOpenEdit" :closeModalEdit="closeModalEdit" :postId="selectedPostId" />
+      <EditPostModal
+        :isOpenEdit="isModalOpenEdit"
+        :closeModalEdit="closeModalEdit"
+        :postId="selectedPostId"
+      />
     </div>
   </div>
 </template>
